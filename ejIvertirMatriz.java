033322f7b17fc7f30744768dcb7027b436a196c3
eddym <eddy@curl.cl>
2022-05-16 16:33:09 -0500

    arregle el metodo para la matriz

--- ejIvertirMatriz.java
+++ ejIvertirMatriz.java
@@ -3,16 +3,21 @@ import java.util.*;
 public class  ejIvertirMatriz {
     public static void main(String[] args) {
         int [] lista = {1,2,3};
-        int [] liA=new int[lista.length];
+        int [] Ain= Minvertida(lista);
+        
+        System.out.println("matriz original: "+ Arrays.toString(lista));
+        System.out.println("matriz invertida: "+ Arrays.toString(Ain));
+    }
+    static int [] Minvertida(int [] lista){  
         int cuenta=0;
-       
+        int [] liA=new int[lista.length];
         for(int i=lista.length-1;i>=0;i--){
             
             liA[cuenta]=lista[i];
             cuenta++;
         }
-        System.out.println("matriz original: "+ Arrays.toString(lista));
-        System.out.println("matriz invertida: "+ Arrays.toString(liA));
+        return liA;
+        
 
     }
 }
