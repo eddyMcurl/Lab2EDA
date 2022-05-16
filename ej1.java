c78a79455113df05970edcf7f4b2446780c20a42
eddym <eddy@curl.cl>
2022-05-16 14:44:58 -0500

    agregue variables y peticion de datos

---
+++ ej1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ej1   {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int entero;
+        int[] lista={1,2,3,4,5};
+        System.out.println("ingresa numero");
+        entero=sc.nextInt();
+
+
+        
+    }
+}
