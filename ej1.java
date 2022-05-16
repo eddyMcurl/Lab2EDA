1bd54cefbfd51628d6111cde341e691449c9d35e
eddym <eddy@curl.cl>
2022-05-16 16:29:31 -0500

    corregi error en el metodo

--- ej1.java
+++ ej1.java
@@ -4,23 +4,24 @@ import javax.xml.stream.events.EndElement;
 public class ej1   {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
-        int aux,entero;
+        int entero;
         int[] lista={1,2,3,4,5};
         System.out.println("ingresa numero");
         entero=sc.nextInt();
+        int []Mrotado = rotarIzquierdaArray(lista);
+        System.out.println("matriz rotada: "+ Arrays.toString(Mrotado));
 
+    }
+    public static int [] rotarIzquierdaArray(int [] lista){
+        int aux;
         for (var i=0;i<lista.length-1;i++){
             aux=lista[1+i];
             lista[1+i]=lista[0];
             lista[0]=aux;
 
         }
-        for(int i=0;i<lista.length;i++)
-        {
-            System.out.print(lista[i]+",");
-        }
-
-
+        return lista;
+    
         
     }
 }
